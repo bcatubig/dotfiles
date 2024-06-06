d268a2d05bb4a6425b589e1a1fc358799cd4710f
Brandon Catubig <bcatubig@pm.me>
2024-06-06 07:11:15 -0700

    wezterm: format

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,16 +1,18 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
-config.color_scheme = 'nordfox'
+config.color_scheme = "nordfox"
 config.window_background_opacity = 0.95
-config.font = wezterm.font('JetBrainsMono Nerd Font', {weight= "Medium"})
+config.font = wezterm.font("JetBrainsMono Nerd Font", {
+    weight = "Medium"
+})
 config.font_size = 16
 config.mouse_wheel_scrolls_tabs = false
-config.window_close_confirmation = 'NeverPrompt'
+config.window_close_confirmation = "NeverPrompt"
 config.audible_bell = "Disabled"
 config.window_decorations = "RESIZE"
 config.scrollback_lines = 10000
@@ -20,91 +22,104 @@ config.tab_bar_at_bottom = false
 
 -- Dim inactive panes
 config.inactive_pane_hsb = {
-  saturation = 0.25,
-  brightness = 0.5
+    saturation = 0.25,
+    brightness = 0.5
 }
 
-
-config.keys = {
-	{
-		key = '%',
-		mods = 'CTRL|SHIFT',
-		action = wezterm.action.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
-	},
-	{
-		key = '"',
-		mods = 'CTRL|SHIFT',
-		action = wezterm.action.SplitVertical({ domain = 'CurrentPaneDomain' }),
-	},
-  {
+config.keys = {{
+    key = "%",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.SplitHorizontal({
+        domain = "CurrentPaneDomain"
+    })
+}, {
+    key = '"',
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.SplitVertical({
+        domain = "CurrentPaneDomain"
+    })
+}, {
     key = "d",
     mods = "SUPER|SHIFT",
-  	action = wezterm.action.SplitVertical({ domain = 'CurrentPaneDomain' }),
-  }
-}
+    action = wezterm.action.SplitVertical({
+        domain = "CurrentPaneDomain"
+    })
+}}
 
 wezterm.on("update-status", function(window, pane)
-  -- Workspace name
-  local stat = window:active_workspace()
-  local stat_color = "#f7768e"
-  -- It's a little silly to have workspace name all the time
-  -- Utilize this to display LDR or current key table name
-  if window:active_key_table() then
-    stat = window:active_key_table()
-    stat_color = "#7dcfff"
-  end
-  if window:leader_is_active() then
-    stat = "LDR"
-    stat_color = "#bb9af7"
-  end
-
-  local basename = function(s)
-    -- Nothing a little regex can't fix
-    return string.gsub(s, "(.*[/\\])(.*)", "%2")
-  end
-
-  -- Current working directory
-  local cwd = pane:get_current_working_dir()
-  if cwd then
-    if type(cwd) == "userdata" then
-      -- Wezterm introduced the URL object in 20240127-113634-bbcac864
-      cwd = basename(cwd.file_path)
-    else
-      -- 20230712-072601-f4abf8fd or earlier version
-      cwd = basename(cwd)
+    -- Workspace name
+    local stat = window:active_workspace()
+    local stat_color = "#f7768e"
+    -- It's a little silly to have workspace name all the time
+    -- Utilize this to display LDR or current key table name
+    if window:active_key_table() then
+        stat = window:active_key_table()
+        stat_color = "#7dcfff"
     end
-  else
-    cwd = ""
-  end
+    if window:leader_is_active() then
+        stat = "LDR"
+        stat_color = "#bb9af7"
+    end
 
-  -- Current command
-  local cmd = pane:get_foreground_process_name()
-  -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
-  cmd = cmd and basename(cmd) or ""
+    local basename = function(s)
+        -- Nothing a little regex can't fix
+        return string.gsub(s, "(.*[/\\])(.*)", "%2")
+    end
 
-  -- Time
-  local time = wezterm.strftime("%H:%M")
+    -- Current working directory
+    local cwd = pane:get_current_working_dir()
+    if cwd then
+        if type(cwd) == "userdata" then
+            -- Wezterm introduced the URL object in 20240127-113634-bbcac864
+            cwd = basename(cwd.file_path)
+        else
+            -- 20230712-072601-f4abf8fd or earlier version
+            cwd = basename(cwd)
+        end
+    else
+        cwd = ""
+    end
 
-  -- Left status (left of the tab line)
-  window:set_left_status(wezterm.format({
-    { Foreground = { Color = stat_color } },
-    { Text = "  " },
-    { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
-    { Text = " |" },
-  }))
+    -- Current command
+    local cmd = pane:get_foreground_process_name()
+    -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
+    cmd = cmd and basename(cmd) or ""
 
-  -- Right status
-  window:set_right_status(wezterm.format({
-    -- Wezterm has a built-in nerd fonts
+    -- Time
+    local time = wezterm.strftime("%H:%M")
+
+    -- Left status (left of the tab line)
+    window:set_left_status(wezterm.format({{
+        Foreground = {
+            Color = stat_color
+        }
+    }, {
+        Text = "  "
+    }, {
+        Text = wezterm.nerdfonts.oct_table .. "  " .. stat
+    }, {
+        Text = " |"
+    }}))
+
+    -- Right status
+    window:set_right_status(wezterm.format({ -- Wezterm has a built-in nerd fonts
     -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
-    { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
-    { Text = " | " },
-    { Foreground = { Color = "#e0af68" } },
-    { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
-    "ResetAttributes",
-    { Text = " | " },
-    { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
-    { Text = "  " },
-  }))
+    {
+        Text = wezterm.nerdfonts.md_folder .. "  " .. cwd
+    }, {
+        Text = " | "
+    }, {
+        Foreground = {
+            Color = "#e0af68"
+        }
+    }, {
+        Text = wezterm.nerdfonts.fa_code .. "  " .. cmd
+    }, "ResetAttributes", {
+        Text = " | "
+    }, {
+        Text = wezterm.nerdfonts.md_clock .. "  " .. time
+    }, {
+        Text = "  "
+    }}))
 end)
 return config
