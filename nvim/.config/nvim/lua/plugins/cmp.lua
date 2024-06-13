5bd45c7cd5e2e422272888cd49ad0722e8e50876
Brandon Catubig <bcatubig@pm.me>
2024-06-13 14:07:49 -0700

    nvim: add cmp symbol_text

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -128,13 +128,20 @@ return {
         },
         formatting = {
           format = lspkind.cmp_format {
-            mode = 'symbol',
+            mode = 'symbol_text',
             maxwidth = 50,
             ellipsis_char = '...',
             show_labelDetails = true,
             before = function(entry, vim_item)
               return vim_item
             end,
+            menu = {
+              buffer = '[Buffer]',
+              nvim_lsp = '[LSP]',
+              luasnip = '[LuaSnip]',
+              nvim_lua = '[Lua]',
+              latex_symbols = '[Latex]',
+            },
           },
         },
         experimental = {
