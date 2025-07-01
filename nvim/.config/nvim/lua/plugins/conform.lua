92c739c79f70417be56152572c11c47fc967f417
Brandon Catubig <bcatubig@pm.me>
2025-07-01 13:54:22 -0700

    update conform

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,30 +1,66 @@
 return {
   "stevearc/conform.nvim",
+  cmd = { "ConformInfo" },
   event = { "BufWritePre" },
-  opts = {
-    notify_on_error = false,
-    formatters_by_ft = {
-      astro = { "prettier" },
-      sh = { "shfmt" },
-      bash = { "shfmt" },
-      lua = { "stylua" },
-      go = { "goimports", "gofumpt" },
-      python = { "ruff_organize_imports", "ruff_format" },
-      yaml = { "prettier" },
-      json = { "prettier" },
-      markdown = { "prettier" },
-      javascript = { "prettier" },
-      html = { "prettier" },
-      css = { "prettier" },
-      toml = { "prettier" },
-      terraform = { "terraform_fmt" },
-      sql = { "sqlfluff" },
-      ["*"] = { "trim_newlines", "trim_whitespace" },
-    },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = "fallback",
+  keys = {
+    {
+      "<leader>f",
+      function()
+        require("conform").format({ async = true })
+      end,
+      desc = "Format buffer",
     },
   },
-  cmd = { "ConformInfo" },
+  config = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+    require("conform").setup({
+      notify_on_error = false,
+      formatters_by_ft = {
+        astro = { "prettier" },
+        sh = { "shfmt" },
+        zsh = { "shfmt" },
+        bash = { "shfmt" },
+        lua = { "stylua" },
+        go = { "goimports", "gofumpt" },
+        python = { "ruff_organize_imports", "ruff_format" },
+        yaml = { "prettier" },
+        json = { "prettier" },
+        markdown = { "prettier" },
+        javascript = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        toml = { "prettier" },
+        terraform = { "terraform_fmt" },
+        sql = { "sqlfluff" },
+        ["*"] = { "trim_newlines", "trim_whitespace" },
+      },
+
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
+    })
+
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+  end,
 }
