90fbe84c5d4d5d70d1be23d7e9983ebb0dfe9da4
Brandon Catubig <bcatubig@pm.me>
2025-07-02 15:23:20 -0700

    add themes

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -13,4 +13,10 @@ return {
       vim.cmd.colorscheme("onedark")
     end,
   },
+  { "sainnhe/everforest", priority = 1000 },
+  { "EdenEast/nightfox.nvim", priority = 1000 },
+  { "folke/tokyonight.nvim", priority = 1000 },
+  { "rebelot/kanagawa.nvim", priority = 1000 },
+  { "nyoom-engineering/oxocarbon.nvim", priority = 1000 },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", priority = 1000 },
 }
