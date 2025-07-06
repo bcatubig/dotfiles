65844a2d972c71b656f9d871745d49a515a7161a
Brandon Catubig <bcatubig@pm.me>
2025-07-06 13:11:04 -0700

    formatting

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,11 +5,11 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 
 local is_linux = function()
-	return wezterm.target_triple:find("linux") ~= nil
+  return wezterm.target_triple:find("linux") ~= nil
 end
 
 local is_darwin = function()
-	return wezterm.target_triple:find("darwin") ~= nil
+  return wezterm.target_triple:find("darwin") ~= nil
 end
 
 config.color_scheme = "onedarkpro_onedark"
@@ -26,51 +26,51 @@ config.send_composed_key_when_left_alt_is_pressed = false
 -- config.default_domain = "unix"
 
 config.window_padding = {
-	left = 10,
-	right = 10,
-	top = 10,
-	bottom = 10,
+  left = 10,
+  right = 10,
+  top = 10,
+  bottom = 10,
 }
 
 config.keys = {
-	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
-	-- Make Option-Right equivalent to Alt-f; forward-word
-	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
-	{
-		key = "t",
-		mods = "CMD|SHIFT",
-		action = act.ShowTabNavigator,
-	},
-	{
-		key = '"',
-		mods = "CMD|SHIFT",
-		action = act.SplitVertical,
-	},
-	{
-		key = "-",
-		mods = "CMD|SHIFT",
-		action = act.SplitHorizontal,
-	},
-	{
-		key = "UpArrow",
-		mods = "SUPER",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "DownArrow",
-		mods = "SUPER",
-		action = act.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "LeftArrow",
-		mods = "SUPER",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "RightArrow",
-		mods = "SUPER",
-		action = act.ActivatePaneDirection("Right"),
-	},
+  { key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  { key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+  {
+    key = "t",
+    mods = "CMD|SHIFT",
+    action = act.ShowTabNavigator,
+  },
+  {
+    key = '"',
+    mods = "CMD|SHIFT",
+    action = act.SplitVertical,
+  },
+  {
+    key = "-",
+    mods = "CMD|SHIFT",
+    action = act.SplitHorizontal,
+  },
+  {
+    key = "UpArrow",
+    mods = "SUPER",
+    action = act.ActivatePaneDirection("Up"),
+  },
+  {
+    key = "DownArrow",
+    mods = "SUPER",
+    action = act.ActivatePaneDirection("Down"),
+  },
+  {
+    key = "LeftArrow",
+    mods = "SUPER",
+    action = act.ActivatePaneDirection("Left"),
+  },
+  {
+    key = "RightArrow",
+    mods = "SUPER",
+    action = act.ActivatePaneDirection("Right"),
+  },
 }
 
 return config
