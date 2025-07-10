93de5d7c693d6a2da014bc9ad1137062f5f30f8d
Brandon Catubig <bcatubig@pm.me>
2025-07-10 13:35:41 -0700

    add bufferline and nvim-tree

---
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  lazy = false,
+  opts = {},
+  keys = {
+    {
+      "<leader>e",
+      "<cmd>NvimTreeToggle<cr>",
+    },
+  },
+}
