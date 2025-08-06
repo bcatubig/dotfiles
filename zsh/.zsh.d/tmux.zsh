f60300d60aaa23196141946158883ee969af4748
Brandon Catubig <bcatubig@pm.me>
2025-08-06 11:33:49 -0700

    update tnew function

--- zsh/.zsh.d/tmux.zsh
+++ zsh/.zsh.d/tmux.zsh
@@ -6,10 +6,15 @@ function tnew() {
     return
   fi
 
-  tmux new-session -d -s "${session_name}" "nvim" >/dev/null
-  tmux rename-window -t "${session_name}":1 "vim"
-  tmux new-window -t "${session_name}"
-  tmux rename-window -t "${session_name}":2 "terminal"
-  tmux select-window -t "${session_name}":1
-  tmux a -t "${session_name}"
+  if tmux list-sessions | grep "${session_name}"; then
+    tmux attach-session -t "${session_name}"
+  else
+    tmux new-session -d -s "${session_name}"
+    tmux rename-window -t "${session_name}":1 "nvim"
+    tmux send-keys -t "${session_name}:1" 'nvim' Enter
+    tmux new-window -t "${session_name}"
+    tmux rename-window -t "${session_name}":2 "terminal"
+    tmux select-window -t "${session_name}":1
+    tmux a -t "${session_name}"
+  fi
 }
