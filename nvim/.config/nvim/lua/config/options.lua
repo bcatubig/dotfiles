33702f658b0226a76707a38335063f2401a905ae
Brandon Catubig <bcatubig@pm.me>
2025-11-03 10:37:18 -0800

    updates

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,4 +1,4 @@
-local opt = vim.opt
+local opt = vim.o
 
 opt.relativenumber = true
 opt.tabstop = 2
@@ -27,3 +27,4 @@ opt.confirm = true
 opt.termguicolors = true
 opt.winborder = "rounded"
 -- opt.winborder = "single"
+opt.confirm = true
