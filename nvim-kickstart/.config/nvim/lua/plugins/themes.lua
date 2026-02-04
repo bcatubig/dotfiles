c1c91d00600ea69557bdb389889f5239c6df63af
Brandon Catubig <bcatubig@pm.me>
2026-02-04 10:56:14 -0800

    update kickstart

---
+++ nvim-kickstart/.config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
+      -- vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+  {
+    'olimorris/onedarkpro.nvim',
+    config = function()
+      require('onedarkpro').setup {
+        options = {
+          transparency = true,
+          cursorline = true,
+        },
+      }
+
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+}
---
+++ nvim-kickstart/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,38 @@
+return { -- Highlight, edit, and navigate code
+  'nvim-treesitter/nvim-treesitter',
+  config = function()
+    local filetypes = {
+      'astro',
+      'bash',
+      'c',
+      'css',
+      'go',
+      'diff',
+      'helm',
+      'html',
+      'ini',
+      'jinja',
+      'jinja_inline',
+      'java',
+      'javascript',
+      'lua',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'properties',
+      'python',
+      'query',
+      'vim',
+      'vimdoc',
+      'terraform',
+      'typescript',
+      'toml',
+      'yaml',
+    }
+    require('nvim-treesitter').install(filetypes)
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = filetypes,
+      callback = function() vim.treesitter.start() end,
+    })
+  end,
+}
