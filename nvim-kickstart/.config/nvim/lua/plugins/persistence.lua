f2e8c90686d338128ca0080eb401184db15230b7
Brandon Catubig <bcatubig@pm.me>
2026-02-10 10:36:19 -0800

    add keys

--- nvim-kickstart/.config/nvim/lua/plugins/persistence.lua
+++ nvim-kickstart/.config/nvim/lua/plugins/persistence.lua
@@ -2,5 +2,10 @@ return {
   'folke/persistence.nvim',
   event = 'BufReadPre', -- this will only start session saving when an actual file was opened
   opts = {},
-  keys = {},
+  keys = {
+    { '<leader>qs', function() require('persistence').load() end, desc = 'load session in current directory' },
+    { '<leader>qS', function() require('persistence').select() end, desc = 'select session' },
+    { '<leader>ql', function() require('persistence').load { last = true } end, desc = 'load session' },
+    { '<leader>qd', function() require('persistence').stop() end, desc = 'Dont save session' },
+  },
 }
